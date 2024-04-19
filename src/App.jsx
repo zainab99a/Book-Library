93c816c5e5b510d45b821911be65f1d40a9e6f5b
zainab99a <amanhal729@gmail.com>
2024-04-19 12:36:14 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [input, setinput] = useState('')
+  const [books, setbooks] = useState([])
+  useEffect(()=>{
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=danbrown/search?q=${input}`)
+    .then((res) => {
+      return res.json(); })
+    .then((data) => {
+      setbooks(data.items);});
+
+  },[input]);
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setinput(event.target.value);
+    }
+  };
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <input className='search'type="text"
+     placeholder='Search' onChange={(e)=>setinput(e.target.value)} 
+     value={input} onKeyDown={handleKeyPress}/>
+    <div className='container'>
+      {books.map(book => (
+        <div className="card">
+          {book.volumeInfo.imageLinks && (
+      <img className='image' src={book.volumeInfo.imageLinks.smallThumbnail} alt="" />
+    )}
+          <h5>{book.volumeInfo.title}</h5>
+          <p>{book.volumeInfo.subtitle}</p>
+          
+        </div>
+      ))}
+      </div>
+    
     </div>
+    
   );
 }
 
